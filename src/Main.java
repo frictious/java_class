b190ff8f9ceb956abf18ed6e03541ec80398c2fd
frictious <gegbaid@gmail.com>
2023-06-05 22:11:00 +0000

    First push

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Single line Comments
+        /**
+         * This is going to be the first class we will take
+         * using an intelligent IDE
+         */
+
+
+    }
+}
