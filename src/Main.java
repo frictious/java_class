0a2d7902d6da1c88da858ce2b29e118f4c29d3d2
frictious <gegbaid@gmail.com>
2023-06-05 22:47:26 +0000

    Finished ide class

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,65 @@ public class Main {
          * using an intelligent IDE
          */
 
+        /**
+         * Data Types
+         * Long
+         * Short
+         * Int
+         * Byte
+         * Float
+         * Double
+         * Boolean
+         * Char
+         */
+
+        // Variable is a name for a storage location in memory
+
+        // Declaration of variables
+        byte age;
+
+        // Initialization of variables
+        age = 32;
+
+        // Printing the value stored in the variable
+        System.out.println(age); // Printing the value of age
+        System.out.println("Hi Ezekiel, your age is " + age); // Concatenation (joining strings and variables together)
+
+        // Declaration of variables
+        byte firstNumber, secondNumber;
+        int answer;
+
+        // Initialization of variables
+        firstNumber = 43;
+        secondNumber = 78;
+        answer = firstNumber + secondNumber;
+        System.out.println(firstNumber + " + " + secondNumber + " = " + answer);
+
+        System.out.println("\n\n\n\n");
+
+        // CLASS EXERCISE
+        // Declaration of variables
+        int fn, sn;
+        int ans;
+
+        // Initialization of variables
+        fn = 300;
+        sn = 100;
+        // ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+        ans = fn + sn;
+        System.out.println("Addition: " + ans);
+        ans = fn - sn;
+        System.out.println("Subtraction: " + ans);
+        ans = fn * sn;
+        System.out.println("Multiplication: " + ans);
+        ans = sn / fn; // Whole number
+        System.out.println("Division: " + ans + "." + (sn % fn));
+        // ans = sn % fn; // Remainder
+        // System.out.println("Remainder: " + ans);
+
+
 
     }
+
+
 }
